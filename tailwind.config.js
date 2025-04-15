e0e0891a30b591805352b984b91a02b703737b36
dowonee <jjddwwyt@gmail.com>
2025-04-15 17:13:47 +0900

    chore: tailwind CSS 설정 (#1)

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#2536D2",
+        accent: "#6C63FF",
+        subaccent: "#A0AEC0",
+        background: "#F3F6FF",
+      },
+    },
+  },
+  plugins: [],
+};
