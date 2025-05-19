0eb8992282168446f7f134a0f570107c42d04839
dowonee <jjddwwyt@gmail.com>
2025-05-19 09:38:09 +0900

    feat: 옵션 페이지 진입점 HTML 파일 추가

---
+++ src/options/main.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import Options from "./Options.jsx";
+
+const root = createRoot(document.getElementById("root"));
+root.render(<Options />);
